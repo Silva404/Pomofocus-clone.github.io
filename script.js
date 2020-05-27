37d28b9a2ee1a9b591b9c1833c9a63e0880a009f
Silva404 <brenosilva0252@gmail.com>
2020-05-27 07:56:50 -0300

    teste

--- script.js
+++ script.js
@@ -3,6 +3,21 @@
 <input type="text" name="name" id="nome" class="popo"/>
 <button class="botao">eae</button>
 
+
+//02 html
+<a href="Acessar meu log">eaeaeae</a>
 // SCRIPT
 let inputElement = document.querySelector("input [name=nome]");
 let eae = document.querySelector('button.botao')
+
+
+//02 script
+// document.createElement  => cria uma espécie de elemento html
+let linkELement = document.createElement('a');
+// .setAttibute => adiciona parametros ao meu elemento criado
+linkELement.setAttribute('href', 'https://eaeaea.com');
+
+// .createTextNode cria um texto que pode ser depois associado a um elemento já criado.
+let textLink = document.createTextNode('Acessar meu log');
+// .appendChild associa um elemento a outro, deixando ele como elemento filho.
+linkELement.appendChild(textLink);
