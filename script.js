b60010d9d517cfb5d58ead5a7378b710862b635b
Silva404 <brenosilva0252@gmail.com>
2020-05-27 12:21:02 -0300

    teste

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
-let 
+let listElement = document.querySelector('#app ul');
+let inputElement = document.querySelector('#app input');
+let taskElement = document.querySelector('#app #add-task');
+let saveElement = document.querySelector('#app #options #save');
+let deleteElement = document.querySelector('#app #options #delete');
+let cancelElement = document.querySelector('#app #options #cancel');
+
+let todos = [];
+
+function renderTodos(){
+  listElement.innerHTML = '';
+
+  for (todo of todos) {
+    let todoElement = document.createElement('li');
+    let todoText = document.createTextNode(todo);
+
+
+
+    todoElement.appendChild(todoText);
+    listElement.appendChild(todoElement);
+  }
+}
+
+function addTodo () {
+  if (inputElement.value != '' && inputElement.value != null) {
+    let todoText = inputElement.value;
+    todos.push(todoText);
+  } 
+
+  inputElement.value = '';
+  renderTodos();
+}
+
+function deleteTodo(item) {
+  todos.splice(item, 1)
+  renderTodos();
+}
+
+function switchDivs() {
+    let e = document.querySelector('#app #tasks #add-task');
+    let a = document.querySelector('#box');
+    let i = document.querySelector('#box #cancel');
+
+    if (e.style.display === "none") {
+      e.style.display = "block";
+      a.style.display = "none";
+    } else {
+      e.style.display = "none";
+      a.style.display = "block";
+    }
+}
+
+
+
+// só serve quando já tiver todos salvo e armazenados.
+// renderTodos();
+taskElement.onclick = switchDivs;
+taskElement.onclick = switchDivs;
+saveElement.onclick = addTodo;
+deleteElement.onclick = deleteTodo;
