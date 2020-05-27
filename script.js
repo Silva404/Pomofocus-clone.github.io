e07c700329472f9938bdd34cb424a63c9a822e4c
Silva404 <brenosilva0252@gmail.com>
2020-05-27 07:45:20 -0300

    teste

---
+++ script.js
@@ -0,0 +1,8 @@
+// HTML
+
+<input type="text" name="name" id="nome" class="popo"/>
+<button class="botao">eae</button>
+
+// SCRIPT
+let inputElement = document.querySelector("input [name=nome]");
+let eae = document.querySelector('button.botao')
