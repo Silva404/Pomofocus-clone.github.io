de388cdc6ee1dc00c8abf0e7c10dc5093b722b75
Silva404 <brenosilva0252@gmail.com>
2020-05-28 17:23:42 -0300

    terminei quase todo o front end

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const cancelElement = document.querySelector('#app #options #cancel');
 const pomodoroElement = document.querySelector('#breaks #pomodoro');
 const shortElement = document.querySelector('#breaks #short');
 const longElement = document.querySelector('#breaks #long');
+const startElement = document.querySelector('div.timer #start')
 
 
 let todos = [];
@@ -18,13 +19,9 @@ function renderTodos(){
   for (todo of todos) {
     let todoElement = document.createElement('li');
     let todoText = document.createTextNode(todo);
-    // let pomo = document.createTextNode('div todo');
-    let icon = document.createElement('div');
-
-    
+    let icon = document.createElement('div');   
 
     todoElement.appendChild(todoText);
-    // todoElement.appendChild(pomo);
     todoElement.appendChild(icon);
     listElement.appendChild(todoElement);
   }
@@ -62,18 +59,29 @@ function switchDivs() {
     }
 }
 
-function switchBColor() {
-  
+function changeBcColor(color) {
+  let liBackground = document.querySelector('div.timer #breaks li');
+  document.body.style.backgroundColor = "color";
+  liBackground.style.backgroundColor = "#5554541a"
+}
+
+function startOrStop(){
+  if (startElement.innerText == "START") startElement.innerText = "END";
+  else startElement.innerText = "START";
 }
 
 // só serve quando já tiver todos salvo e armazenados.
 // renderTodos();
 
-pomodoroElement.onclick = switchBColor;
-longElement.onclick = switchBColor;
-shortElement.onclick = switchBColor;
+//botão start ou stop
+startElement.onclick = startOrStop;
 
+//mudando a cor dos backgrounds
+pomodoroElement.onclick = () => document.body.style.backgroundColor = "#ce092399";;
+longElement.onclick = () => document.body.style.backgroundColor = "#11588899";
+shortElement.onclick = () => document.body.style.backgroundColor = "#00746e99";
 
+// funcionalidade do tasks
 cancelElement.onclick = switchDivs;
 taskElement.onclick = switchDivs;
 saveElement.onclick = addTodo;
